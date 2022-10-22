c6bd9f4e42fb0d691a4b4f8c9c49e253a156d779
theomelgar <theomelgar@gmail.com>
2022-10-22 18:29:24 -0300

    Embaralhar/Numero de cartas pares

--- codigos/codigo.js
+++ codigos/codigo.js
@@ -1,23 +1,54 @@
-const numeroCartas = [];
+let quantidadeCartas = Number(prompt('Quntas cartas?(4-14)'));
 
-const quantidadeCartas = Number(prompt('Quntos pares de cartas?'));
+let versos = ['bobross','explody','fiesta', 'metal', 'revertit','triplets','unicorn'];
 
-let contador = 0;
-while (quantidadeCartas > contador){
-    numeroCartas.push();
-    contador ++;
+while ( quantidadeCartas & 1){
+    quantidadeCartas = Number(prompt('Quntos pares de cartas?(4-14)'));
 }
 
-const posicaoCarta = document.querySelector('.jogo');
+while((quantidadeCartas/2) < 2 || (quantidadeCartas/2) > 7){
+    quantidadeCartas = Number(prompt('Quntos pares de cartas?(4-14)'));
+} 
 
-contador = 0 ;
 
-while(quantidadeCartas > contador){
+let posicaoCarta = document.querySelector('.jogo');
+
+let contador = 0;
+
+while((quantidadeCartas/2) > contador){
     posicaoCarta.innerHTML += `
-    <div class="carta">
-    <img src="imagens/back.png">
+    <div class="carta" data-framework="${versos[contador]}">
+        <img class="verso" src="/imagens/back.png">
+        <img class="frente " src="/cartas/${versos[contador]}parrot.gif">
+    </div>
+    <div class="carta" data-framework="${versos[contador]}">
+        <img class="verso" src="/imagens/back.png">
+        <img class="frente " src="/cartas/${versos[contador]}parrot.gif">
     </div>
     `;
 
     contador ++;
 }
+  
+const cartas = document.querySelectorAll(".carta");
+
+
+function virar(){
+    this.classList.add("virada");
+}
+
+
+(function embaralhar() {
+
+    cartas.forEach(carta => {
+ 
+      let ramdomPos = Math.floor(Math.random() * 14);
+ 
+      carta.style.order = ramdomPos;
+ 
+    });
+ 
+  })();
+
+cartas.forEach(carta => { carta.addEventListener('click', virar)
+});
